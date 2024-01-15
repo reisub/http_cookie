617c0048231bbf0634b05a3fa55f56ec3eeecdf1
Dino Kovač <dino@latinum.xyz>
2024-01-15 23:23:22 +0100

    Add GH Actions config

--- mix.exs
+++ mix.exs
@@ -5,10 +5,11 @@ defmodule HttpCookie.MixProject do
     [
       app: :http_cookie,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -22,13 +23,24 @@ defmodule HttpCookie.MixProject do
     [
       {:idna, "~> 6.1"},
       {:public_suffix, github: "axelson/publicsuffix-elixir"},
-      {:nimble_parsec, "~> 1.0", optional: true}
+      {:nimble_parsec, "~> 1.0", optional: true},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
   defp aliases do
     [
       compile_parser: "nimble_parsec.compile lib/http_cookie/date_parser.ex.exs"
+    ]
+  end
+
+  def dialyzer do
+    [
+      # Put the project-level PLT in the priv/ directory (instead of the default _build/ location)
+      plt_file: {:no_warn, "priv/plts/project.plt"},
+
+      # Also put the core Erlang/Elixir PLT into the priv/ directory like so:
+      plt_core_path: "priv/plts/core.plt"
     ]
   end
 end
